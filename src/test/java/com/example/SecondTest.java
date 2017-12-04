4aa7ef07b9d0b5698465f0aedfb6482b7fb2a56d
Will Schipp <3287360+willschipp@users.noreply.github.com>
2017-12-04 17:47:56 +0400

    Update and rename OtherPageSeleniumTest.java to SecondTest.java

--- src/test/java/com/example/SecondTest.java
+++ src/test/java/com/example/SecondTest.java
@@ -13,7 +13,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes=Application.class)
-public class OtherPageSeleniumTest {
+public class SecondTest {
 
 	@Before
 	public void before() throws Exception {
