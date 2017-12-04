f2b0693ffd94de2281e0038d2ffa2236073fbfbc
Will Schipp <3287360+willschipp@users.noreply.github.com>
2017-12-04 17:47:39 +0400

    Update and rename HomePageSeleniumTest.java to FirstTest.java

--- src/test/java/com/example/FirstTest.java
+++ src/test/java/com/example/FirstTest.java
@@ -13,7 +13,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes=Application.class)
-public class HomePageSeleniumTest {
+public class FirstTest {
 
 	@Before
 	public void before() throws Exception {
